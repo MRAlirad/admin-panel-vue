f1d74215ab1da1605e410718493d4d08a3b0894e
MRAlirad <m.r.alirad75@gmail.com>
2023-09-18 01:30:14 +0330

    icon

--- src/App.vue
+++ src/App.vue
@@ -1,13 +1,33 @@
-<template>
-	<Icon icon="attach_money" />
-</template>
-<script>
-	import Icon from './components/Icon.vue';
-
-	export default {
-		components: {
-			Icon,
-		},
-	};
+<script setup>
+	import SideBar from './components/side-bar/SideBar.vue';
 </script>
-<style scoped></style>
+
+<template>
+	<div class="admin-panel-app bg-[#F4F7FE] min-h-screen grid grid-cols-[max-content_1fr] ">
+		<SideBar />
+	</div>
+</template>
+
+<style>
+	@font-face {
+		font-family: 'Material Icons';
+		font-style: normal;
+		font-weight: 400;
+		src: url('./assets/MaterialIcons.woff2') format('woff2');
+	}
+	.material-icons {
+		font-family: 'Material Icons';
+		font-weight: normal;
+		font-style: normal;
+		font-size: 24px;
+		line-height: 1;
+		letter-spacing: normal;
+		text-transform: none;
+		display: inline-block;
+		white-space: nowrap;
+		word-wrap: normal;
+		direction: ltr;
+		/* -webkit-font-feature-settings: 'liga'; */
+		-webkit-font-smoothing: antialiased;
+	}
+</style>
