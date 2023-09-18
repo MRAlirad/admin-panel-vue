7bfc08127b523b01c539d85208103937ab0c2204
MRAlirad <m.r.alirad75@gmail.com>
2023-09-18 21:09:47 +0330

    header

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -1,5 +1,11 @@
 <template>
-    <div class="flex items-center justify-between w-full h-[180px]">
-        
+    <div class="flex items-center justify-between w-full h-[180px] px-4">
+        <div class="page-title-box">
+            <div class="navigation"> صفحات / پروفایل</div>
+            <h1>پروفایل</h1>
+        </div>
+        <div class="settings-search-box h-16 bg-white rounded-[30px] w-[420px]">
+
+        </div>
     </div>
 </template>
