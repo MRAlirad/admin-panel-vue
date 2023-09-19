ec2a2aef58151fe0b56565a0fbc2b3bd5669f943
MRAlirad <m.r.alirad75@gmail.com>
2023-09-19 18:28:14 +0330

    kanban - tasks

--- src/pages/Kanban.vue
+++ src/pages/Kanban.vue
@@ -1,3 +1,30 @@
 <template>
-    Kanban
+    <div class="kanban-page grid grid-cols-[1fr_1fr_1fr] gap-[1%]">
+        <div class="pending-tasks-container card px-9 py-6 ">
+            <div class="title-box flex items-center justify-between">
+                <h2 class="text-[22px] text-delftBlue font-bold">در انتظار تایید</h2>
+                <button class="btn w-[90px] h-[35px] text-blue bg-ghostWhite rounded-xl">
+                    <i class="material-icons material-icons-round"> add </i>
+                </button>
+            </div>
+        </div>
+
+        <div class="running-tasks-container card px-9 py-6 ">
+            <div class="title-box flex items-center justify-between">
+                <h2 class="text-[22px] text-delftBlue font-bold">در حال انجام</h2>
+                <button class="btn w-[90px] h-[35px] text-blue bg-ghostWhite rounded-xl">
+                    <i class="material-icons material-icons-round"> add </i>
+                </button>
+            </div>
+        </div>
+
+        <div class="done-task-container card px-9 py-6 ">
+            <div class="title-box flex items-center justify-between">
+                <h2 class="text-[22px] text-delftBlue font-bold">انجام شده</h2>
+                <button class="btn w-[90px] h-[35px] text-blue bg-ghostWhite rounded-xl">
+                    <i class="material-icons material-icons-round"> add </i>
+                </button>
+            </div>
+        </div>
+    </div>
 </template>
