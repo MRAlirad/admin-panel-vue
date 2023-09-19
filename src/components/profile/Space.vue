e08a865a546768b6b0986a96a5551b2481c39063
MRAlirad <m.r.alirad75@gmail.com>
2023-09-19 17:39:24 +0330

    space

--- src/components/profile/Space.vue
+++ src/components/profile/Space.vue
@@ -1,16 +1,19 @@
 <template>
-    <div class="your-space-segment card p-4 pb-8">
-        <ProgressBar :remain="50" :total="100" />
-    </div>
+	<div class="your-space-segment card p-4 pb-8">
+		<ProgressBar
+			:remain="50"
+			:total="100"
+		/>
+	</div>
 </template>
 
 <script>
-    import ProgressBar from '../ProgressBar.vue';
+	import ProgressBar from '../ProgressBar.vue';
 	export default {
-        components: {
-            ProgressBar,
-        }
-    };
+		components: {
+			ProgressBar,
+		},
+	};
 </script>
 
 <style></style>
